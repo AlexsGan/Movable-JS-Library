92cfaa15c2af418675cd24ad1fb3222e67d85912
alexgan <alex.gan@mail.utoronto.ca>
2020-07-21 14:15:41 -0400

    created an express app with static directory middleware

---
+++ express/pub/js/Movable.js
@@ -0,0 +1,179 @@
+/* JS Library */
+"use strict";
+// This file can be found by a client request to /js/Movable.js which happens through
+// the <script> tag in examples.html.
+// The static directory middleware gives the client access to this file.
+
+console.log('Js libraries')
+
+// Different way of creating library: creating an object constructor and then
+// adding to its prototype.
+
+// Movable Library
+
+function Grid(numBoxes) {
+    this.boxes = [];
+    // this.positions = [[]];
+
+    for (let i = 0; i < numBoxes; i++) {
+        const mb = new MovableBox(i, numBoxes);
+        this.boxes.push(mb);
+    }
+
+    const gridContainer = document.createElement("div");
+    gridContainer.id = "grid";
+
+    for (let i = 0; i < numBoxes; i++) {
+        gridContainer.appendChild(this.boxes[i].dragItem)
+        this.boxes[i].dragItem.style.left = i * 300 + 'px';
+        this.boxes[i].dragItem.style.top = 0 + 'px';
+        // this.positions[i][0] = this.boxes[i].dragItem.style.left
+        // this.positions[i][1] = this.boxes[i].dragItem.style.up
+    }
+
+    const body = document.querySelector('body')
+    body.append(gridContainer)
+
+    // for (let i = 0; i < numBoxes; i++) {
+    //     this.boxes[i].setEmpties(this.boxes);
+    // }
+
+}
+
+Grid.prototype = {
+
+}
+
+function MovableBox(i, numBoxes) {
+    // this..
+    // this.. (any values you need for each 'instance' of this library)
+
+    const box = document.createElement("div");
+    box.id = "box";
+
+    this.dragItem = box;
+    // this.empties = document.querySelectorAll('#item');
+
+    this.active = false;
+    this.initialX;
+    this.initialY;
+    this.currentX;
+    this.currentY;
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.content = "box " + i;
+    var _this = this;
+
+    // this.dragItem.style.left = i*180+'px';
+    // this.dragItem.style.top = 0+'px';
+
+    this.dragItem.innerHTML = this.content;
+
+    this.dragItem.addEventListener("touchstart", dragStart, false);
+    this.dragItem.addEventListener("touchend", dragEnd, false);
+    this.dragItem.addEventListener("touchmove", drag, false);
+
+    this.dragItem.addEventListener("mousedown", dragStart, false);
+    this.dragItem.addEventListener("mouseup", dragEnd, false);
+    this.dragItem.addEventListener("mousemove", drag, false);
+
+    var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    this.dragItem.style.backgroundColor = randomColor;
+
+    const body = document.querySelector('body')
+    body.append(this.dragItem)
+    // this.boxes.push(box)
+
+    function dragStart(e) {
+        if (e.type === "mousedown") {
+            _this.initialX = e.clientX - _this.offsetX;
+            _this.initialY = e.clientY - _this.offsetY;
+        } else {
+            _this.initialX = e.touches[0].clientX - _this.offsetX;
+            _this.initialY = e.touches[0].clientY - _this.offsetY;
+        }
+
+        if (e.target === _this.dragItem) {
+            _this.active = true;
+        }
+    }
+
+    function dragEnd(e) {
+        let x = 0;
+        let y = 0;
+        for (let i = 0; i < numBoxes; i++) {
+            if (_this.offsetX > 300 * i - 150 && _this.offsetX < 300 * i + 150) { // if moved to right
+                x = 300 * i;
+                // console.log(_this.offsetX+" x")
+                break;
+            }
+            else if (_this.offsetX < - 300 * i + 150 && _this.offsetX > - 300 * i - 150) { // if moved to left
+                x = - 300 * i;
+                // console.log(_this.offsetX+" x")
+                break;
+            }
+        }
+        for (let i = 0; i < numBoxes; i++) {
+            let s = 0;
+            if (_this.offsetY > 440 * i - 220 && _this.offsetY < 440 * i + 220) { // if moved down
+                y = 440 * i;
+                // console.log(_this.offsetY+" y")
+                break;
+            }
+            else if (_this.offsetY < - 440 * i + 220 && _this.offsetY > - 440 * i - 220) { // if moved up
+                y = - 440 * i;
+                // console.log(_this.offsetY+" y")
+                break;
+            }
+        }
+        _this.currentX = x;
+        _this.currentY = y;
+        _this.dragItem.style.transform = "translate3d(" + x + "px, " + y + "px, 0)"; // set box to the closest legal location
+
+        _this.initialX = _this.currentX;
+        _this.initialY = _this.currentY;
+
+        _this.offsetX = _this.currentX;
+        _this.offsetY = _this.currentY;
+
+        _this.active = false;
+    }
+
+    function drag(e) {
+        if (_this.active) {
+            e.preventDefault();
+            if (e.type === "mousemove") {
+                _this.currentX = e.clientX - _this.initialX;
+                _this.currentY = e.clientY - _this.initialY;
+            }
+            else {
+                _this.currentX = e.touches[0].clientX - _this.initialX;
+                _this.currentY = e.touches[0].clientY - _this.initialY;
+            }
+
+            _this.offsetX = _this.currentX;
+            _this.offsetY = _this.currentY;
+
+            _this.dragItem.style.transform = "translate3d(" + _this.currentX + "px, " + _this.currentY + "px, 0)";
+        }
+    }
+}
+
+// Added common funcionality to the prototype property of the constructor.
+MovableBox.prototype = {
+
+    setBoxContent: function (content) {
+        // this.dragItem.innerHTML = content;
+        this.dragItem.appendChild(content);
+        // const body = document.querySelector('body')
+        // body.append(content);
+    },
+
+    setBoxBackgroundColor: function (color) {
+        this.dragItem.style.backgroundColor = color;
+    },
+
+    // setEmpties: function (boxes) {
+    //     this.empties = boxes;
+    // }
+}
---
+++ express/pub/js/examples.js
@@ -0,0 +1,50 @@
+/* dom content loaded */
+"use strict";
+// This file can be found by a client request to /js/Movable.js which happens through
+// the <script> tag in examples.html.
+// The static directory middleware gives the client access to this file.
+
+const log = console.log
+log('Loading DOM Content')
+
+function logId(element) {
+	log(element.id)
+}
+
+// below are example usage of the library
+
+// create a grids with 4 boxes each
+const grid = new Grid(4);
+const grid2 = new Grid(4);
+
+// const mb = new MovableBox(1, 4);
+
+
+// adding pure text and setting colour
+const content0 = document.createElement("div");
+content0.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+grid.boxes[0].setBoxContent(content0);
+grid.boxes[0].setBoxBackgroundColor("pink");
+
+// adding an image and a text
+const content1 = document.createElement("div");
+const imgDescription = document.createElement("div");
+imgDescription.innerHTML = "Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+const img = document.createElement("img");
+img.src = "individual_pic_1.png";
+content1.appendChild(img);
+content1.appendChild(imgDescription);
+
+grid.boxes[1].setBoxContent(content1);
+
+// adding an image and a text
+const content2 = document.createElement("div");
+const imgDescription2 = document.createElement("div");
+imgDescription2.innerHTML = "Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+const img2 = document.createElement("img");
+img2.src = "individual_pic_2.jpg";
+content2.appendChild(img2);
+content2.appendChild(imgDescription2);
+
+grid.boxes[3].setBoxContent(content2);
